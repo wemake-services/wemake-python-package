025bdefedd1460c85dd1107072af21f1300f824c
sobolevn <mail@sobolevn.me>
2025-04-25 13:01:31 +0300

    Fix CI

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -38,7 +38,7 @@ def generate_license() -> None:
         mode='w',
         encoding='utf8',
     ) as license_file:
-        license_file.write(license_result.strip())
+        license_file.write(license_result.strip().replace(' \n ', ' \n'))
         license_file.write('\n')
 
 
